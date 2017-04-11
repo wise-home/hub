a4a398acd77580e1505a93f7e021df60904c7fef
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2017-04-11 23:18:34 +0200

    Added application

--- mix.exs
+++ mix.exs
@@ -15,7 +15,10 @@ defmodule Hub.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [
+      extra_applications: [:logger],
+      mod: {Hub.Application, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
