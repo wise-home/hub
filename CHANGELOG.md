4c8ed825f2b99378f628f03fe3fbe304fa95b4bd
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2017-04-18 16:27:05 +0200

    Prepare for 0.2.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.0 - 2017-04-18
+
+* Allow local variables to be pinned in pattern [#3](https://github.com/vesta-merkur/hub/pull/3)
+
 ## 0.1.0 - 2017-04-14
 
 First released version.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hub.Mixfile do
   def project do
     [
       app: :hub,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
